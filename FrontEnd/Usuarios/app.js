79ebedaa36901d2c07da089f22faad522f798085
Unknown <fmnozcstro@outlook.com>
2018-03-15 20:51:31 -0600

    cambios vista
    cambios vista

--- FrontEnd/Usuarios/app.js
+++ FrontEnd/Usuarios/app.js
@@ -1,11 +1,6 @@
 angular.module('appModule',["ngRoute"])
     .config(['$routeProvider',function($routeProvider){
         $routeProvider
-            .when('/', {
-                templateUrl: "Views/secretary.html",
-                controller: "secretaryCtrl"
-            })
-        /*
             .when('/registro_funcionarios', {
                 templateUrl: "Views/login.html",
                 controller: "loginCtrl"
@@ -26,6 +21,7 @@ angular.module('appModule',["ngRoute"])
             /*.when('/', {
                 templateUrl: "Views/registration_official.html",
                 controller: "officialCtrl"
+            })*/
             .otherwise({
                 redirectTo: '/'
             });
