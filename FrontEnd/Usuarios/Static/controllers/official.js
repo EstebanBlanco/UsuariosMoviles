d7e820a82e251700e71005ecdbf6a02f2938da3b
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-03-22 20:56:26 -0500

    Mejorando datepicker
    - falta placeholder para mostrar fecha actual
    - falta extraer unicamente la fecha

--- FrontEnd/Usuarios/Static/controllers/official.js
+++ FrontEnd/Usuarios/Static/controllers/official.js
@@ -48,6 +48,14 @@ var app = angular.module('appModule')
             }
         ];
 
+        $scope.getCurrentDate = function () {
+            var dt = new Date();
+            var month = dt.getMonth()+1;
+            var day = dt.getDate();
+            var year = dt.getFullYear();
+            document.getElementById("dateB").value = day + "/" + month + "/" + year;
+        };
+
         //verify the checked roles
         $scope.roleList = [];
         $scope.roleChecked = function() {
